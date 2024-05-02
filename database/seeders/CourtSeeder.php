cae44ba3450298a4ffce252b160cc201f3936990
Mario <marpul3@hotmail.com>
2024-05-02 21:09:23 +0200

    Añadido Tarifas aSeeder, nombre pista modificado

--- database/seeders/CourtSeeder.php
+++ database/seeders/CourtSeeder.php
@@ -14,42 +14,42 @@ class CourtSeeder extends Seeder
     public function run(): void
     {
         $court1 = new Court();
-        $court1->name = 'Pista 1';
+        $court1->name = 'PADEL 1 CI';
         $court1->type = 'Padel';
         $court1->save();
 
         $court2 = new Court();
-        $court2->name = 'Pista 2';
+        $court2->name = 'PADEL 2 CI';
         $court2->type = 'Padel';
         $court2->save();
 
         $court3 = new Court();
-        $court3->name = 'Pista 3';
+        $court3->name = 'PADEL 3 CI';
         $court3->type = 'Padel';
         $court3->save();
 
         $court4 = new Court();
-        $court4->name = 'Pista 4';
+        $court4->name = 'PADEL 4 MI';
         $court4->type = 'Padel';
         $court4->save();
 
         $court5 = new Court();
-        $court5->name = 'Pista 5';
+        $court5->name = 'PADEL 5 CE';
         $court5->type = 'Padel';
         $court5->save();
 
         $court6 = new Court();
-        $court6->name = 'Pista 6';
+        $court6->name = 'PADEL 6 ME';
         $court6->type = 'Padel';
         $court6->save();
 
         $court7 = new Court();
-        $court7->name = 'Pista 7';
+        $court7->name = 'PADEL 7 ME';
         $court7->type = 'Padel';
         $court7->save();
 
         $court8 = new Court();
-        $court8->name = 'Pista 8';
+        $court8->name = 'Pista 8 ME';
         $court8->type = 'Padel';
         $court8->save();
 
--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -9,7 +9,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 class SectionSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * SECCIONES HORARIAS
+     * 
      */
     public function run(): void
     {
@@ -42,5 +43,85 @@ class SectionSeeder extends Seeder
         $section6->start_time = '11:30:00';
         $section6->end_time = '12:00:00';
         $section6->save();
+
+        $section7 = new Section();
+        $section7->start_time = '12:00:00';
+        $section7->end_time = '12:30:00';
+        $section7->save();
+
+        $section8 = new Section();
+        $section8->start_time = '12:30:00';
+        $section8->end_time = '13:00:00';
+        $section8->save();
+
+        $section9 = new Section();
+        $section9->start_time = '13:00:00';
+        $section9->end_time = '13:30:00';
+        $section9->save();
+
+        $section10 = new Section();
+        $section10->start_time = '13:30:00';
+        $section10->end_time = '14:00:00';
+        $section10->save();
+
+        $section11 = new Section();
+        $section11->start_time = '14:00:00';
+        $section11->end_time = '14:30:00';
+        $section11->save();
+
+        $section12 = new Section();
+        $section12->start_time = '14:30:00';
+        $section12->end_time = '15:00:00';
+        $section12->save();
+
+        $section13 = new Section();
+        $section13->start_time = '15:00:00';
+        $section13->end_time = '15:30:00';
+        $section13->save();
+
+        $section14 = new Section();
+        $section14->start_time = '15:30:00';
+        $section14->end_time = '16:00:00';
+        $section14->save();
+
+        $section15 = new Section();
+        $section15->start_time = '16:00:00';
+        $section15->end_time = '16:30:00';
+        $section15->save();
+
+        $section16 = new Section();
+        $section16->start_time = '16:30:00';
+        $section16->end_time = '17:00:00';
+        $section16->save();
+
+        $section17 = new Section();
+        $section17->start_time = '17:00:00';
+        $section17->end_time = '17:30:00';
+        $section17->save();
+
+        $section18 = new Section();
+        $section18->start_time = '17:30:00';
+        $section18->end_time = '18:00:00';
+        $section18->save();
+
+        $section19 = new Section();
+        $section19->start_time = '18:00:00';
+        $section19->end_time = '18:30:00';
+        $section19->save();
+
+        $section20 = new Section();
+        $section20->start_time = '18:30:00';
+        $section20->end_time = '19:00:00';
+        $section20->save();
+
+        $section21 = new Section();
+        $section21->start_time = '19:00:00';
+        $section21->end_time = '19:30:00';
+        $section21->save();
+
+        $section22 = new Section();
+        $section22->start_time = '19:30:00';
+        $section22->end_time = '20:00:00';
+        $section22->save();
     }
 }
